efe851d2469d851a7f88ff8c8d5e4ecc446bcb0d
WjahatAhmad <wajahat5ahmad@gmail.com>
2022-09-03 00:41:17 +0500

    fixed mouse position

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-/** type @type {HtmlCanvasElement} */
+/** @type {HTMLCanvasElement} */
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const canvasPosition = canvas.getBoundingClientRect();
+
+const CANVAS_WIDTH = (canvas.width = 500);
+const CANVAS_HEIGHT = (canvas.height = 800);
+
+let x = undefined;
+let y = undefined;
+
+
+
+
+window.addEventListener("click", (evt) => {
+  x = evt.clientX - canvasPosition.left;
+  y = evt.clientY - canvasPosition.top;
+  ctx.fillStyle = "white";
+  ctx.fillRect(x, y, 50, 50);
+});
+
+// const animate = () => {
+//   requestAnimationFrame(animate);
+
+// }
+
+// animate();
